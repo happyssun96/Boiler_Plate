da6cd98663d811160ab871ee214d516e27298af1
happyssun96 <lsw96@naver.com>
2021-01-13 22:21:00 +0900

    Redux Setting

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -1,17 +1,12 @@
-import React, { useEffect } from 'react'
-import axios from 'axios'
+import React, { useEffect } from "react";
+import axios from "axios";
 
 function LandingPage() {
-    useEffect(() => {
-        axios.get('/api/hello')
-        .then(response => console.log(response.data))
-    }, [])
+  useEffect(() => {
+    axios.get("/api/hello").then((response) => console.log(response.data));
+  }, []);
 
-    return (
-        <div>
-            LandingPage 랜딩 페이지
-        </div>
-    )
+  return <div>LandingPage 랜딩 페이지</div>;
 }
 
-export default LandingPage
+export default LandingPage;
